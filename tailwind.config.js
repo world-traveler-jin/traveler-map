b81fa9b90fecf0753eb781e68f433833aa43b4db
seongjin kim <mansj98@naver.com>
2025-04-24 23:38:36 +0900

    Fixed build issues and updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './styles/**/*.css', // 글로벌 CSS 경로 추가
   ],
   theme: {
     extend: {},
